32b5e3c101cc212b8eb8b023c7aca6f8910dd59c
kbachmann1556 <kbachmann91@gmail.com>
2016-12-02 15:12:49 -0800

    testing submodules

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var fs = require('fs');
 
 mongoose.connect('mongodb://localhost/Parker');
-
+a change in a different spot
 var models_path = __dirname + '/../server/models'
 
 fs.readdirSync(models_path).forEach(function (file){
